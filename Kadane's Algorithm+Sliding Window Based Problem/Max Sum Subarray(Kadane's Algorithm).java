a5e472c14742ff28b512d54ca34d751c28722140
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-07-11 14:39:03 +0530

    Update Max Sum Subarray(Kadane's Algorithm).java

--- Kadane's Algorithm+Sliding Window Based Problem/Max Sum Subarray(Kadane's Algorithm).java
+++ Kadane's Algorithm+Sliding Window Based Problem/Max Sum Subarray(Kadane's Algorithm).java
@@ -1,3 +1,28 @@
+/*Brute Force Approach:
+TC-O(n*n)
+SC-O(1)*/
+class Solution{
+
+    int maxSubarraySum(int arr[], int n){
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            sum=0;
+            for(int j=i;j<n;j++)
+            {
+                sum=sum+arr[j];
+                max=Math.max(max,sum);
+            }
+        }
+        return max;
+    }
+    
+}
+
+/*Optimal Approach
+TC-O(n)
+SC-o(1)*/
 public int maxSubArray(int[] nums) {
         int sum=0;
         int max=Integer.MIN_VALUE;
