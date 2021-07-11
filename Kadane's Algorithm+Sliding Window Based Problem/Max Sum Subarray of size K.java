3ac09b58579db02cddbe969db22909496ab89524
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-07-11 14:23:18 +0530

    Update Max Sum Subarray of size K.java

--- Kadane's Algorithm+Sliding Window Based Problem/Max Sum Subarray of size K.java
+++ Kadane's Algorithm+Sliding Window Based Problem/Max Sum Subarray of size K.java
@@ -1,3 +1,33 @@
+/*Brute Force Approach:
+TC-O(n*k)
+SC-O(1)*/
+
+class Solution{
+    static int maximumSumSubarray(int K, ArrayList<Integer> Arr,int n){
+        int max=0;
+        int sum=0;
+        for(int i=0;i<=n-K;i++)
+        {
+            sum=0;
+            for(int j=i;j<i+K;j++)
+            {
+                sum=sum+Arr.get(j);
+            }
+            max=Math.max(max,sum);
+        }
+        return max;
+    }
+}
+
+
+
+
+
+
+
+/* Optimal Approach:
+TC-O(n)
+SC-o(1)*/
 class Solution{
     static int maximumSumSubarray(int k, ArrayList<Integer> Arr,int N){
         
