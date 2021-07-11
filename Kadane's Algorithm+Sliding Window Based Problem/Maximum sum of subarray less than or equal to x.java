df8597a51dd711111973f5fa70224066aa105fc9
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-07-11 14:32:24 +0530

    Update Maximum sum of subarray less than or equal to x.java

--- Kadane's Algorithm+Sliding Window Based Problem/Maximum sum of subarray less than or equal to x.java
+++ Kadane's Algorithm+Sliding Window Based Problem/Maximum sum of subarray less than or equal to x.java
@@ -1,3 +1,40 @@
+/*Brute Force Approach:
+TC-O(n*n)
+SC-O(1)*/
+
+class Solution
+{
+    long findMaxSubarraySum(long arr[], int N,int X)
+    {
+        long sum=0;
+        long max=0;
+        int startIndex=0;
+        for(int i=0;i<N;i++)
+        {
+            sum=0;
+            for(int j=i;j<N;j++)
+            {
+              sum=sum+arr[j];
+              if(sum<=X)
+              {
+                  max=Math.max(max,sum);
+              }
+            }
+        }
+        return max;
+    }
+}
+
+
+
+
+
+
+
+
+/*Optimal Approach:
+TC-O(n)
+SC-o(1)*/
 class Solution
 {
     long findMaxSubarraySum(long arr[], int N,int X)
