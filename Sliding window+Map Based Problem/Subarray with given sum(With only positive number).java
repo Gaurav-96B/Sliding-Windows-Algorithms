563cc0c3d8ea1bd469a44f657f48a91cf14f733d
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-07-11 13:18:29 +0530

    Update Subarray with given sum(With only positive number).java

--- Sliding window+Map Based Problem/Subarray with given sum(With only positive number).java
+++ Sliding window+Map Based Problem/Subarray with given sum(With only positive number).java
@@ -1,34 +1,34 @@
-
+/*Brute Force Approach:
+TC-O(n*n)
+SC-O(1)*/
 class Solution
 {
     static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
     {
         int sum=0;
-        ArrayList<Integer>a=new ArrayList<>();
-        int startIndex=0;
+        ArrayList<Integer>ans=new ArrayList<>();
         for(int i=0;i<n;i++)
         {
-          sum=sum+arr[i];
-          while(sum>s)
-          {
-              sum=sum-arr[startIndex];
-              startIndex++;
-              
-          }
-          if(sum==s)
-          {
-            a.add(startIndex+1);
-            a.add(i+1);
-            return a;
-          }
+            sum=0;
+           for(int j=i;j<n;j++)
+           {
+               sum=sum+arr[j];
+               if(sum==s)
+               {
+                  ans.add(i+1);
+                  ans.add(j+1);
+                  return ans;
+               }
+           }
         }
-        a.add(-1);
-        return a;
+        ans.add(-1);
+        return ans;
     }
 }
 
-
-
+/*Optimal Approach:
+TC-O(n)
+SC-O(1)*/
 class Solution
 {
     static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
